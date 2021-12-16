48fc10fd4ca5e521853bfa561e2b5e9efd29b93c
Zhuo Liu <zhuol@mit.edu>
2021-12-16 12:16:36 -0500

    plots

--- Diagnostics/local/movie_make.py
+++ Diagnostics/local/movie_make.py
@@ -4,7 +4,7 @@ import glob
 
 file_array = []
 img_array = []
-for filename in glob.glob('./Diagnostics/local/Ratio/mass400/dist_function/IAT_E2_f2D_*.png'):
+for filename in glob.glob('./Diagnostics/local/Ratio/mass400/dist_function/IAT_E2_f1D_*.png'):
     file_array.append(filename)
 
 file_array.sort()
@@ -15,7 +15,7 @@ for filename in file_array[:]:
     img_array.append(img)
  
  
-out = cv2.VideoWriter('./output_video_e2_2d2.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
+out = cv2.VideoWriter('./output_video_e2_1d.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
  
 for image in img_array:
     out.write(image)
