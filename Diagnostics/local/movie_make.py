119cf6eed9e6a978a91769afcfe001566efa6273
Zhuo Liu <zhuol@mit.edu>
2022-10-06 14:36:32 -0400

    update

--- Diagnostics/local/movie_make.py
+++ Diagnostics/local/movie_make.py
@@ -4,7 +4,7 @@ import glob
 
 file_array = []
 img_array = []
-for filename in glob.glob('./Diagnostics/local/Cori/mass25/rescheck/4/dist_function/M25_E2_3_f2D_*.png'):
+for filename in glob.glob('./Diagnostics/local/massRatio/mass25/E1/dist_function_plots/M25_E1_f1D_*.png'):
     file_array.append(filename)
 
 file_array.sort()
@@ -14,7 +14,7 @@ for filename in file_array[:]:
     size = (width,height)
     img_array.append(img)
  
-out = cv2.VideoWriter('./output_video_e2_1d.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
+out = cv2.VideoWriter('./output_video_e1_1d.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 5, size)
  
 for image in img_array:
     out.write(image)
